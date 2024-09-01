15b3a79bf84b2147c4242e784d2ed0a8cf1155ab
kwetso-ogodiseng-kgaladi1 <kgaladikwetso@gmail.com>
2024-09-01 17:46:42 +0200

    updated

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 // Middleware to parse incoming JSON requests
 app.use(express.json());
 
-// Define the POST endpoint at /sort-string
+// Define the POST endpoint at /sort
 app.post('/sort', (req, res) => {
     // Extract the 'data' field from the request body
     const { data } = req.body;
 
-    // Check if 'data' is a string
+    // Check if 'data' is string
     if (typeof data !== 'string') {
         // If not, return a 400 error with an error message
         return res.status(400).json({ error: 'Invalid input.' });
@@ -23,7 +23,7 @@ app.post('/sort', (req, res) => {
     res.json({ word: sortedWord });
 });
 
-// Start the server and listen on port 3000
+// Start the server 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
